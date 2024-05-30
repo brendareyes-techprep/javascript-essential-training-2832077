1335682d42ed630b916939ee8c8f50632cee849e
Brenda Reyes <129559415+brendareyes-techprep@users.noreply.github.com>
2024-05-30 21:41:50 +0000

    bracket

--- 03_06/script.js
+++ 03_06/script.js
@@ -24,3 +24,5 @@ const backpack = {
 console.log("The backpack object:", backpack);
 console.log("The pocketNum value:", backpack.pocketNum);
 console.log("Strap length L:", backpack.strapLength.left);
+
+console.log("The pocketNum value:", backpack["pocketNum"]);
