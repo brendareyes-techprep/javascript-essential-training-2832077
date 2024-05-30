868811530382c7933bb9ee8169fbe32bf6fb5bb3
Brenda Reyes <129559415+brendareyes-techprep@users.noreply.github.com>
2024-05-30 21:43:02 +0000

    query

--- 03_06/script.js
+++ 03_06/script.js
@@ -25,4 +25,6 @@ console.log("The backpack object:", backpack);
 console.log("The pocketNum value:", backpack.pocketNum);
 console.log("Strap length L:", backpack.strapLength.left);
 
-console.log("The pocketNum value:", backpack["pocketNum"]);
+var query = "pocketNum";
+
+console.log("The pocketNum value:", backpack[query]);
